d80a1f974b29c652ff02c888e02f78baab104f57
Pavin Pankajan <pavinpankajan@Pavins-MacBook-Air.local>
2025-11-10 07:28:54 +0530

    sitemap updated

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
+import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sampadavr.com',
   output: 'static',
+  integrations: [
+    sitemap({
+      filenameBase: 'sitemap',
+    }),
+  ],
 });
